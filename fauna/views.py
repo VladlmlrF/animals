9423273994c620d43af1133e0fcf383762f724d5
VladlmlrF <vladimirfalin84@gmail.com>
2023-07-29 12:23:10 +0700

    create base.html, index.html, fauna.urls.py

--- fauna/views.py
+++ fauna/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Animal
+
+
+class AnimalHome(ListView):
+    model = Animal
+    template_name = 'fauna/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['menu'] = menu
+        context['title'] = 'Главная страница'
+        # context['cat_selected'] = 0
+        return context
+
